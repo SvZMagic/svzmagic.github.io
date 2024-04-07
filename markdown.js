e08aa673653a2560c825d5c017fbd21e5b0c2e18
ArduCat <149066998+SvZMagic@users.noreply.github.com>
2024-04-07 19:26:22 +0300

    РАБОТАЙ, ####!

--- markdown.js
+++ markdown.js
@@ -3,9 +3,11 @@ console.log("[Owlory] Trying to render NEWS...")
 
 
 var i = 1;
+window.onload = function() {
 fetch("news/" + i + "n.md").then((res) => res.text()).then((text) => {
      document.getElementById('nc' + i).innerHTML = marked.parse(text);
 }).catch((e) => console.error(e));
+}
 
 console.log("[Owlory] Check " + i + ' News Container')
 
